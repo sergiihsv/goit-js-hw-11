819bded32025f2c536b1292c0cb5972488a309a8
sergiihsv <hsv-serg@ukr.net>
2022-01-18 01:23:37 +0200

    hw-11 start

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './css/styles.css';
 import PhotoCardTpl from './templates/photo-card.hbs';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+};
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '25290744-d6d0934bf026089ed7a084fd9';
+
+  fetch(
+    'https://pixabay.com/api/?key=25290744-d6d0934bf026089ed7a084fd9&q=cat&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40',
+  )
+    .then(r => r.json())
+    .then(console.log);
+}
